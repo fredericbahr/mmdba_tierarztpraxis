0ea248c9772e08ac4e958d164229e4eca9ce9b0a
fredericbahr <fredericbahr1@gmail.com>
2022-06-06 21:02:23 +0200

    [mmdb-11] add routes and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Layout } from "./components/Layout/Layout";
+import { Animal } from "./pages/Animal/Animal";
+import { Customer } from "./pages/Customer/Customer";
 import { Home } from "./pages/Home/Home";
+import { Medicine } from "./pages/Medicine/Medicine";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="animal" element={<Animal />} />
+          <Route path="medicine" element={<Medicine />} />
+          <Route path="customer" element={<Customer />} />
         </Route>
       </Routes>
     </div>
---
+++ client/src/pages/Animal/Animal.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Animal = () => {
+  return <div>Animal</div>;
+};
